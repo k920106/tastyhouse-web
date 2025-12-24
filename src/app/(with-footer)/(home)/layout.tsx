2a756519b3395cdbba38edabea75ced1a05d6115
kk920106 <kk920106@naver.com>
2025-12-25 07:37:33 +0900

    refactor(HomeLayout): Header 브라우저 경고

--- src/app/(with-footer)/(home)/layout.tsx
+++ src/app/(with-footer)/(home)/layout.tsx
@@ -5,7 +5,16 @@ export default function HomeLayout({ children }: { children: React.ReactNode }) 
   return (
     <>
       <Header>
-        <Image src="/images/header-logo.png" alt="로고" width={83} height={43} priority />
+        <div className="relative w-[83px] h-[43px]">
+          <Image
+            src="/images/header-logo.png"
+            alt="로고"
+            fill
+            sizes="83px"
+            style={{ objectFit: 'contain' }}
+            priority
+          />
+        </div>
       </Header>
       <main className="pb-[70px]">{children}</main>
     </>
