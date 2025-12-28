4df164ec054d93837dc60511e8b0853152ad2454
kk920106 <kk920106@naver.com>
2025-12-29 00:05:50 +0900

    refactor(rank): FixedPositionWrapper를 제거하고 해당 기능을 MyRankFixedSection에 직접 통합하여 구성 요소 구조와 성능을 향상

--- src/app/(with-footer)/rank/_components/MyRankFixedSection.tsx
+++ src/app/(with-footer)/rank/_components/MyRankFixedSection.tsx
@@ -1,19 +1,15 @@
 import { RankPeriod } from '@/types/api/rank'
 import { Suspense } from 'react'
-import FixedPositionWrapper from './FixedPositionWrapper'
-import MyRankInfo from './MyRankInfo'
-import { RankListItemSkeleton } from './RankList'
+import MyRankInfo, { MyRankInfoSkeleton } from './MyRankInfo'
 
 export default function MyRankFixedSection({ rankPeriod }: { rankPeriod: RankPeriod }) {
   return (
-    <FixedPositionWrapper className="bottom-[72px]">
-      <section>
-        <div className="flex justify-between items-center py-[15px] pl-8 pr-[35px] bg-[#eeeeee] border border-[#cccccc] box-border rounded-[2.5px]">
-          <Suspense fallback={<RankListItemSkeleton />}>
-            <MyRankInfo rankPeriod={rankPeriod} />
-          </Suspense>
-        </div>
-      </section>
-    </FixedPositionWrapper>
+    <section className="fixed left-0 right-0 bottom-[72px] w-full">
+      <div className="flex justify-between items-center py-[15px] pl-8 pr-[35px] bg-[#eeeeee] border border-[#cccccc] box-border rounded-[2.5px]">
+        <Suspense fallback={<MyRankInfoSkeleton />}>
+          <MyRankInfo rankPeriod={rankPeriod} />
+        </Suspense>
+      </div>
+    </section>
   )
 }
--- src/app/(with-footer)/rank/_components/MyRankInfo.tsx
+++ src/app/(with-footer)/rank/_components/MyRankInfo.tsx
@@ -1,4 +1,5 @@
 import ErrorMessage from '@/components/ui/ErrorMessage'
+import { Skeleton } from '@/components/ui/shadcn/skeleton'
 import { api } from '@/lib/api'
 import { COMMON_ERROR_MESSAGES } from '@/lib/constants'
 import { API_ENDPOINTS } from '@/lib/endpoints'
@@ -6,6 +7,29 @@ import { ApiResponse } from '@/types/api/api'
 import { MyRankItem, RankPeriod, rankPeriodToRankType } from '@/types/api/rank'
 import { cookies } from 'next/headers'
 import RankItem from './RankItem'
+
+export function MyRankInfoSkeleton() {
+  return (
+    <>
+      <div className="flex gap-2.5">
+        <div className="flex items-center flex-shrink-0 w-[22px]">
+          <Skeleton className="w-4 h-3" />
+        </div>
+        <div className="flex-shrink-0">
+          <Skeleton className="w-10 h-10 rounded-full" />
+        </div>
+        <div className="flex flex-col gap-1 min-w-0">
+          <Skeleton className="w-30 h-5" />
+          <div className="flex items-center gap-[5px]">
+            <Skeleton className="w-5 h-4" />
+            <Skeleton className="w-15 h-3" />
+          </div>
+        </div>
+      </div>
+      <Skeleton className="w-10 h-3" />
+    </>
+  )
+}
 
 export default async function MyRankInfo({ rankPeriod }: { rankPeriod: RankPeriod }) {
   const cookieStore = await cookies()
