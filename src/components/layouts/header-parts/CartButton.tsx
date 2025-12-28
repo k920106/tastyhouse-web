5d106429e3fe846a46a9ec23907af286d11a090d
kk920106 <kk920106@naver.com>
2025-12-28 15:56:57 +0900

    feat(CartButton):  컴포넌트 생성

---
+++ src/components/layouts/header-parts/CartButton.tsx
@@ -0,0 +1,24 @@
+'use client'
+
+import Image from 'next/image'
+
+interface CartButtonProps {
+  count?: number
+  onClick?: () => void
+}
+
+export default function CartButton({ count = 0, onClick }: CartButtonProps) {
+  return (
+    <button
+      onClick={onClick}
+      className="w-[55px] h-[55px] flex items-center justify-center cursor-pointer"
+    >
+      <div className="relative w-[22px] h-[22px] flex items-center justify-center">
+        <Image src="/images/icon-cart.png" alt="장바구니" width={22} height={22} className="z-1" />
+        <span className="absolute top-1.5 flex items-center justify-center w-4 h-4 text-[10px] text-white bg-main">
+          {count > 99 ? '99+' : count}
+        </span>
+      </div>
+    </button>
+  )
+}
