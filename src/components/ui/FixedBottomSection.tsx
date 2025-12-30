9db113cb462092bae69299a497af7ee027c02b15
kk920106 <kk920106@naver.com>
2025-12-30 21:17:23 +0900

    feat(ui): FixedBottomSection 컴포넌트 생성

---
+++ src/components/ui/FixedBottomSection.tsx
@@ -0,0 +1,16 @@
+import { ReactNode } from 'react'
+
+interface FixedBottomSectionProps {
+  children: ReactNode
+  className?: string
+}
+
+export default function FixedBottomSection({ children, className = '' }: FixedBottomSectionProps) {
+  return (
+    <section
+      className={`fixed bottom-0 left-0 right-0 bg-white border-t border-[#eeeeee] box-border ${className}`}
+    >
+      {children}
+    </section>
+  )
+}
