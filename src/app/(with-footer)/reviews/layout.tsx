9dc1c45f2f2eee637989cc49a79a980e864a765a
kk920106 <kk920106@naver.com>
2026-01-01 18:31:00 +0900

    refactor(layout): CartButton 컴포넌트 제외

--- src/app/(with-footer)/reviews/layout.tsx
+++ src/app/(with-footer)/reviews/layout.tsx
@@ -1,5 +1,5 @@
-import Header, { HeaderCenter, HeaderLeft, HeaderRight } from '@/components/layouts/Header'
-import { CartButton, MenuButton } from '@/components/layouts/header-parts'
+import Header, { HeaderCenter, HeaderLeft } from '@/components/layouts/Header'
+import { MenuButton } from '@/components/layouts/header-parts'
 
 export default function ReviewLayout({ children }: { children: React.ReactNode }) {
   return (
@@ -11,9 +11,6 @@ export default function ReviewLayout({ children }: { children: React.ReactNode }
         <HeaderCenter>
           <h1 className="text-[17px] leading-[17px] text-white">리뷰</h1>
         </HeaderCenter>
-        <HeaderRight>
-          <CartButton count={0} />
-        </HeaderRight>
       </Header>
       <div className="pb-[70px]">{children}</div>
     </>
