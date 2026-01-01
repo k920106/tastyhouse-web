81f2a09a8f908f53a0b2baf82e66adc17be0fd22
kk920106 <kk920106@naver.com>
2026-01-01 18:38:07 +0900

    feat(ui): BorderedSection 컴포넌트 생성

---
+++ src/components/ui/BorderedSection.tsx
@@ -0,0 +1,19 @@
+import { ReactNode } from 'react'
+
+interface BorderedSectionProps {
+  children: ReactNode
+  className?: string
+}
+
+export default function BorderedSection({
+  children,
+  className = '',
+}: BorderedSectionProps) {
+  return (
+    <section
+      className={`bg-white border-y border-[#eeeeee] box-border ${className}`}
+    >
+      {children}
+    </section>
+  )
+}
