00104410b2144b83303d194233ed9e91a41f6646
kk920106 <kk920106@naver.com>
2026-01-07 23:52:39 +0900

    refactor(reviews): tab css 수정

--- src/app/(with-footer)/reviews/_components/ReviewTabs.tsx
+++ src/app/(with-footer)/reviews/_components/ReviewTabs.tsx
@@ -38,13 +38,13 @@ export default function ReviewTabs({ initialTab }: ReviewTabsProps) {
       <TabsList className="sticky top-0 w-full h-[50px] rounded-none bg-white z-40 p-0">
         <TabsTrigger
           value="all"
-          className="flex-1 h-full rounded-none border-0 text-sm leading-[14px] text-[#333333]/40 shadow-none data-[state=active]:text-main data-[state=active]:font-bold data-[state=active]:shadow-none data-[state=active]:border-b-2 data-[state=active]:border-main border-b border-[#eeeeee]"
+          className="flex-1 h-full text-sm leading-[14px] text-[#333333]/40 border-0 border-b border-[#eeeeee] rounded-none shadow-none cursor-pointer data-[state=active]:text-main data-[state=active]:font-bold data-[state=active]:shadow-none data-[state=active]:border-b-2 data-[state=active]:border-main"
         >
           전체
         </TabsTrigger>
         <TabsTrigger
           value="following"
-          className="flex-1 h-full rounded-none border-0 text-sm leading-[14px] text-[#333333]/40 shadow-none data-[state=active]:text-main data-[state=active]:font-bold data-[state=active]:shadow-none data-[state=active]:border-b-2 data-[state=active]:border-main border-b border-[#eeeeee]"
+          className="flex-1 h-full text-sm leading-[14px] text-[#333333]/40 border-0 border-b border-[#eeeeee] rounded-none shadow-none cursor-pointer data-[state=active]:text-main data-[state=active]:font-bold data-[state=active]:shadow-none data-[state=active]:border-b-2 data-[state=active]:border-main"
         >
           팔로잉
         </TabsTrigger>
