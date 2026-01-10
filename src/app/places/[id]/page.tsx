32e7f51489299c3b110bf5a4d27c60d0704220cb
kk920106 <kk920106@naver.com>
2026-01-10 10:54:24 +0900

    refactor(places): 사용하지 않는 리뷰 가져오기 함수를 제거하고 변수 선언을 수정

--- src/app/places/[id]/page.tsx
+++ src/app/places/[id]/page.tsx
@@ -5,32 +5,6 @@ import PlaceImageGallery from './_components/PlaceImageGallerySection'
 import PlaceSummarySection from './_components/PlaceSummarySection'
 import PlaceTabSection from './_components/PlaceTabSection'
 
-// async function getPlaceReviews(placeId: string) {
-//   const response = await api.get<PagedCommonResponse<PlaceReviewResponse>>(
-//     `/api/places/v1/${placeId}/reviews`,
-//     {
-//       params: {
-//         page: 0,
-//         size: 10,
-//       },
-//     },
-//   )
-//   if (response.error || !response.data) {
-//     throw new Error(response.error || '리뷰 정보를 불러올 수 없습니다')
-//   }
-//   return response.data.data
-// }
-
-// async function getPlaceReviewStatistics(placeId: string) {
-//   const response = await api.get<CommonResponse<PlaceReviewStatisticsResponse>>(
-//     `/api/places/v1/${placeId}/reviews/statistics`,
-//   )
-//   if (response.error || !response.data) {
-//     throw new Error(response.error || '리뷰 통계 정보를 불러올 수 없습니다')
-//   }
-//   return response.data.data
-// }
-
 interface PlaceDetailPageProps {
   params: Promise<{
     id: string
@@ -40,8 +14,10 @@ interface PlaceDetailPageProps {
 
 export default async function PlaceDetailPage({ params, searchParams }: PlaceDetailPageProps) {
   const { id } = await params
-  const placeId = Number(id)
   const searchParamsData = await searchParams
+
+  const placeId = Number(id)
+
   const initialTab = (searchParamsData.tab || 'info') as 'info' | 'menu' | 'photo'
 
   return (
