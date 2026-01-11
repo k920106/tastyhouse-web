76f8ab1a0d0f5a9db9d27c0d1f54797d778bff0a
kk920106 <kk920106@naver.com>
2026-01-11 12:38:58 +0900

    refactor(products): ProductItem.tsx -> formatNumber 함수 적용

--- src/components/products/ProductItem.tsx
+++ src/components/products/ProductItem.tsx
@@ -1,5 +1,5 @@
 import { Skeleton } from '@/components/ui/shadcn/skeleton'
-import { formatDecimal } from '@/lib/number'
+import { formatDecimal, formatNumber } from '@/lib/number'
 import { PAGE_PATHS } from '@/lib/paths'
 import Image from 'next/image'
 import Link from 'next/link'
@@ -29,19 +29,19 @@ export function ProductItemSkeleton() {
 
 interface ProductItemProps {
   id: number
-  placeName: string
-  name: string
   imageUrl: string
+  name: string
+  placeName: string
   originalPrice: number
-  discountPrice?: number
-  discountRate?: number
+  discountPrice: number
+  discountRate: number
 }
 
 export default function ProductItem({
   id,
-  placeName,
-  name,
   imageUrl,
+  name,
+  placeName,
   originalPrice,
   discountPrice,
   discountRate,
@@ -51,22 +51,18 @@ export default function ProductItem({
       <div className="relative w-[75px] h-[75px] flex-shrink-0 overflow-hidden">
         <Image src={imageUrl} alt={name} fill className="object-cover" sizes="160px" />
       </div>
-      <div className="flex-1 min-w-0 flex flex-col">
+      <div className="flex-1 flex flex-col min-w-0">
         <p className="mb-2.5 text-xs leading-[12px] truncate">{placeName}</p>
         <h3 className="mb-[17px] text-base leading-[16px] truncate">{name}</h3>
         <div className="flex justify-between">
-          {discountRate == null ? (
-            <div className="flex items-end gap-2">
-              <span className="text-base leading-[16px]">{originalPrice.toLocaleString()}원</span>
-            </div>
+          {discountRate === null ? (
+            <span className="text-base leading-[16px]">{formatNumber(originalPrice)}원</span>
           ) : (
             <>
               <div className="flex items-end gap-2">
-                <span className="text-base leading-[16px]">
-                  {discountPrice?.toLocaleString()}원
-                </span>
+                <span className="text-base leading-[16px]">{formatNumber(discountPrice)}원</span>
                 <span className="text-xs leading-[12px] text-[#aaaaaa] line-through">
-                  {originalPrice.toLocaleString()}원
+                  {formatNumber(originalPrice)}원
                 </span>
               </div>
               <div className="text-right flex-shrink-0">
