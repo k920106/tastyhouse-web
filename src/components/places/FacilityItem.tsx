f34e2174317622f994e3906dfa5f078a4a4d8b44
kk920106 <kk920106@naver.com>
2026-01-11 15:19:10 +0900

    feat(places): FacilityItem.tsx -> FacilityItemSkeleton 컴포넌트 생성

--- src/components/places/FacilityItem.tsx
+++ src/components/places/FacilityItem.tsx
@@ -3,6 +3,19 @@
 import { getPlaceAmenityCodeName } from '@/constants/place'
 import { PlaceAmenityListItem } from '@/types/api/place'
 import Image from 'next/image'
+import { Skeleton } from '../ui/shadcn/skeleton'
+
+export function FacilityItemSkeleton() {
+  return (
+    <div
+      className="flex flex-col items-center justify-center px-5 py-[17px] border border-[#eeeeee] box-border"
+      style={{ aspectRatio: '80 / 95' }}
+    >
+      <Skeleton className="w-[38px] h-[38px] mb-[15px]" />
+      <Skeleton className="w-12 h-3" />
+    </div>
+  )
+}
 
 interface FacilityItemContentProps {
   name: string
