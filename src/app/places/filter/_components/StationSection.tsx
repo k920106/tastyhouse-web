e72b1a01c3feb091e3c9c843e4f17b5e780967ae
kk920106 <kk920106@naver.com>
2026-01-25 00:52:57 +0900

    fix(station-section): BorderedSection CSS 수정

--- src/app/places/filter/_components/StationSection.tsx
+++ src/app/places/filter/_components/StationSection.tsx
@@ -1,11 +1,11 @@
-import { Suspense } from 'react'
 import BorderedSection from '@/components/ui/BorderedSection'
+import { Suspense } from 'react'
 import StationContent from './StationContent'
 import { StationSelectorSkeleton } from './StationSelector'
 
 export default function StationSection() {
   return (
-    <BorderedSection className="px-[15px] py-5">
+    <BorderedSection className="px-[15px] py-5 border-t-0">
       <h2 className="mb-[15px] text-sm leading-[14px]">지하철역</h2>
       <Suspense fallback={<StationSelectorSkeleton />}>
         <StationContent />
