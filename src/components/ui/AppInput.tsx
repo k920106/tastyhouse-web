97b00b433134298105a67f772c2973810b553a32
kk920106 <kk920106@naver.com>
2026-02-01 12:51:47 +0900

    fix(ui): CSS 수정 (AppInput.tsx)

--- src/components/ui/AppInput.tsx
+++ src/components/ui/AppInput.tsx
@@ -7,7 +7,7 @@ export default function AppInput({ className, ...props }: AppInputProps) {
   return (
     <Input
       className={cn(
-        'w-full h-[50px] pl-[16px] pr-[40px] py-5 text-sm leading-[14px] border border-[#eeeeee] box-border rounded-none focus-visible:ring-0 focus-visible:border-[#666666]',
+        'w-full h-[50px] pl-[16px] pr-[40px] py-5 text-sm leading-[14px] border border-[#eeeeee] box-border rounded-none shadow-none focus-visible:ring-0 focus-visible:border-[#666666]',
         className,
       )}
       {...props}
