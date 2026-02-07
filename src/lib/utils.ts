5011370d855f2dbd7021bce8cde2c5a06791ad8f
kk920106 <kk920106@naver.com>
2026-02-07 14:50:04 +0900

    feat(utils): formatPhoneNumber 함수 추가

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,17 @@ import { twMerge } from 'tailwind-merge'
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export function formatPhoneNumber(phone: string): string {
+  const digits = phone.replace(/\D/g, '')
+
+  if (digits.length === 11) {
+    return digits.replace(/(\d{3})(\d{4})(\d{4})/, '$1-$2-$3')
+  }
+
+  if (digits.length === 10) {
+    return digits.replace(/(\d{3})(\d{3})(\d{4})/, '$1-$2-$3')
+  }
+
+  return phone
+}
